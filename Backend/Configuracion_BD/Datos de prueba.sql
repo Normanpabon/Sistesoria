618c1584567398aa8e1ff04bdb0a1704ed477e23
Norman P <45250874+Normanpabon@users.noreply.github.com>
2023-04-27 08:06:35 -0500

    Datos de prueba
    Se crea script de sql con datos para prueba:
    
    Usuarios: 2 profesores 5 estudiantes
    
    Asignaturas : 3
    
    Nota:  Falta modificar el autoincremento para la base de datos y adicionar los otros datos de prueba.

---
+++ Backend/Configuracion_BD/Datos de prueba.sql
@@ -0,0 +1,37 @@
+/* Usuarios / Estudiantes y profesores */
+
+INSERT INTO SISTESORIAS.USUARIO(codigo_institucional, nombre, apellido, usuario, correo, rol, hashed_password) VALUES (2216785,
+    'Alejandra', 'Salamandra', 'alejandra.salamandra', 'alejandra.salamandra@uao.edu.co', 1, 'porquemellamoasi2345' );
+
+INSERT INTO SISTESORIAS.USUARIO(codigo_institucional, nombre, apellido, usuario, correo, rol, hashed_password) VALUES (2220202,
+    'Amadeus', 'Mozart', 'amadeus.mozart', 'amadeus.mozart@uao.edu.co', 1, 'Autonoma0202' );
+
+INSERT INTO SISTESORIAS.USUARIO(codigo_institucional, nombre, apellido, usuario, correo, rol, hashed_password) VALUES (2230004,
+    'Cris', 'Dav', 'cris.dav', 'cris.dav@uao.edu.co', 1, 'Auto_moto_si' );
+
+INSERT INTO SISTESORIAS.USUARIO(codigo_institucional, nombre, apellido, usuario, correo, rol, hashed_password) VALUES (2236677,
+    'Felipe', 'Calpa', 'felipe.calpa', 'felipe.calpa@uao.edu.co', 1, 'luna334455' );
+
+INSERT INTO SISTESORIAS.USUARIO(codigo_institucional, nombre, apellido, usuario, correo, rol, hashed_password) VALUES (2207621,
+    'Antonia', 'Castaneda', 'antonia.castaneda', 'antonia.castaneda@uao.edu.co', 1, 'contra_6660' );
+
+
+INSERT INTO SISTESORIAS.USUARIO(codigo_institucional, nombre, apellido, usuario, correo, rol, hashed_password) VALUES (2187890,
+    'Nayibe', 'Guzman', 'nayibe.guzman', 'nayibe.guzman@uao.edu.co', 0, 'gato_rojo9876' );
+
+INSERT INTO SISTESORIAS.USUARIO(codigo_institucional, nombre, apellido, usuario, correo, rol, hashed_password) VALUES (2126578,
+    'Carlos', 'Rosero', 'carlos.rosero', 'carlos.rosero@uao.edu.co', 0, 'Autonoma5566' );
+
+
+/* Asignaturas */ 
+
+INSERT INTO SISTESORIAS.asignatura(codigo_uao, nombre, creditos) VALUES (22010, 'Programacion orientada a objetos', 3);
+
+INSERT INTO SISTESORIAS.asignatura(codigo_uao, nombre, creditos) VALUES (22011, 'Estructuras de datos I', 3);
+
+INSERT INTO SISTESORIAS.asignatura(codigo_uao, nombre, creditos) VALUES (22012, 'Ingenieria de software I', 3);
+
+
+/* Cursos */
+
+INSERT INTO SISTESORIAS.curso()
